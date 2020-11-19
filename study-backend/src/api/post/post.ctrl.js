4436954ffc24b676ac0bcd4d3e51933868ef8f0c
ypyp66 <ypyp66@naver.com>
2020-11-19 20:57:21 +0900

    post - list 작성

--- study-backend/src/api/post/post.ctrl.js
+++ study-backend/src/api/post/post.ctrl.js
@@ -1,6 +1,7 @@
 import Post from '../../models/post';
 
 export const write = async ctx => {
+  //ctx에는 request와 response가 담겨있음
   const { author, title, category, photo, video, content, views, publishedDate } = ctx.request.body;
   const post = new Post({
     //Post객체 생성
@@ -17,13 +18,25 @@ export const write = async ctx => {
     await post.save(); //데이터베이스에 저장하는데 시간이 걸리므로 비동기처리를 해줌
     ctx.body = post;
   } catch (e) {
+    ctx.throw(500, e); //내부서버오류
+  }
+};
+
+export const list = async ctx => {
+  //게시글 조회
+  try {
+    const posts = await Post.find().exec();
+    //find()함수 호출 후 exec()를 붙어주어야 서버에 쿼리를 요청함
+    ctx.body = posts;
+  } catch (e) {
     ctx.throw(500, e);
   }
 };
 
-export const list = ctx => {};
-
-export const read = ctx => {};
+export const read = ctx => {
+  //특정글 읽기(id로 찾아서 조회)
+  const { id } = ctx.params;
+};
 
 export const remove = ctx => {};
 
